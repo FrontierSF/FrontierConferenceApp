58d801171b3e694230ce67be3cffbf494f062b35
Harry Gale <harry.n.gale@gmail.com>
2018-03-17 16:26:26 -0700

    Add styling and icon for app

--- js/features/reg-screen/component.js
+++ js/features/reg-screen/component.js
@@ -1,27 +1,46 @@
 import React from 'react';
 import { Text, View, TouchableOpacity, Image, Dimensions } from 'react-native';
+import FontAwesomeIcon from 'react-native-vector-icons/FontAwesome';
+import { Sae } from 'react-native-textinput-effects';
+
 import PropTypes from 'prop-types';
 import OrangeGradient from '../../shared/components/OrangeGradient';
 import { Colors } from '../../themes';
-import { Button } from 'react-native-elements';
+import { Button, FormValidationMessage } from 'react-native-elements';
 
 import style from './style';
 
 const width = Dimensions.get('window').width;
 
 class RegScreen extends React.Component {
+  //this.setState({text})
   render() {
     const { goToTab } = this.props;
+    const widthInput = width * 0.65
+    // 1) Check if input in state is valid and
+    //
     return (
       <OrangeGradient style={style.container}>
         <Image style={style.logo} source={require('../../shared/assets/footerlogo.png')} />
-        <Text style={style.textSub}>
-          Enter Reg Code
-        </Text>
+        <Sae
+          style={{ width: (widthInput-10), marginTop: 10, marginBottom: 20}}
+          label={'Registration Code'}
+          iconClass={FontAwesomeIcon}
+          iconName={'pencil'}
+          iconColor={'white'}
+          labelStyle={{ color: 'white', fontStyle: 'italic', fontWeight: 'normal' }}
+          inputStyle={{ color: 'white' }}
+          keyboardType="numeric"
+          onChangeText={(text) => { console.log('Keyboard changed ', text);}}
+          // TextInput props
+          autoCapitalize={'none'}
+          autoCorrect={false}
+        />
         <Button
           raised
-          containerViewStyle={{width: width * 0.65 }}
+          containerViewStyle={{width:widthInput}}
           color={Colors.orange}
+          onPress={goToTab}
           borderRadius={10}
           backgroundColor='white'
           title='Sign In' />
